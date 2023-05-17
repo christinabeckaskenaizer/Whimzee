19567805cb238465a5f7c16fd5280f05a33f4ac7
Sina Klughardt <sina.klughardt@gmail.com>
2023-05-17 15:00:37 -0700

    Get all users endpoint implemented

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
+from typing import List, Union
 from queries.users import (Error, UserIn, UserOut, UserRepository)
 
 router = APIRouter()
@@ -9,3 +10,10 @@ async def create(
     repo:UserRepository=Depends()
 ):
     return repo.create(user)
+
+@router.get("/users", response_model = List[UserOut])
+async def get_all(
+    response: Response,
+    repo: UserRepository = Depends()
+):
+    return repo.get_all()
