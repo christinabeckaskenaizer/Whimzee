11f924c6e14c7e75815bef6a48184427a4ffd551
Santiago Bothe <santiagobothe@outlook.com>
2023-05-19 00:30:07 +0000

    Authorization for shops

--- api/routers/shop.py
+++ api/routers/shop.py
@@ -5,6 +5,7 @@ from queries.shop import (
     ShopOut,
     ShopRepository
 )
+from authenticator import authenticator
 
 router = APIRouter()
 
@@ -13,22 +14,16 @@ async def create(
     shop: ShopIn,
     response: Response,
     repo: ShopRepository = Depends(),
+    account: dict = Depends(authenticator.try_get_current_account_data)
 ) -> ShopOut:
-    result = repo.create(shop)
+    if account == None:
+        response.status_code = 401
+        return Error(message="Sign in to access")
+    user_id = account.get("id")
+    result = repo.create(shop, int(user_id))
     if result == None:
         response.status_code = 404
         result = Error(message="Unable to create new shop")
-    return result
-
-@router.get("/shops", response_model = list[ShopOut] | Error)
-async def get_all(
-    response: Response,
-    repo: ShopRepository = Depends()
-):
-    result = repo.get_all()
-    if result == None:
-        response.status_code = 404
-        result = Error(message="Unable to get shops")
     return result
 
 @router.put("/shops/{shop_id}", response_model = ShopOut | Error)
@@ -36,21 +31,31 @@ async def update(
     shop_id: int,
     shop: ShopIn,
     response: Response,
-    repo: ShopRepository = Depends()
+    repo: ShopRepository = Depends(),
+    account: dict = Depends(authenticator.try_get_current_account_data)
 ) -> ShopOut | Error:
-    result = repo.update(shop_id, shop)
+    if account == None:
+        response.status_code = 401
+        return Error(message="Sign in to access")
+    user_id = account.get("id")
+    result = repo.update(shop_id, shop, int(user_id))
     if result == None:
         response.status_code = 404
-        result = Error(message="Unable to get shops")
+        result = Error(message="Unable to update shop")
     return result
 
 @router.delete("/shops/{shop_id}", response_model = bool | Error)
 async def delete(
     shop_id: int,
     response: Response,
-    repo: ShopRepository = Depends()
+    repo: ShopRepository = Depends(),
+    account: dict = Depends(authenticator.try_get_current_account_data)
 ) -> bool | Error:
-    result = repo.delete(shop_id)
+    if account == None:
+        response.status_code = 401
+        return Error(message="Sign in to access")
+    user_id = account.get("id")
+    result = repo.delete(shop_id, int(user_id))
     if result == None:
         response.status_code = 404
         result = Error(message="Invalid shop id")
@@ -66,4 +71,15 @@ async def get_one(
     if result == None:
         response.status_code = 404
         result = Error(message="Invalid shop id")
+    return result
+
+@router.get("/shops", response_model = list[ShopOut] | Error)
+async def get_all(
+    response: Response,
+    repo: ShopRepository = Depends()
+):
+    result = repo.get_all()
+    if result == None:
+        response.status_code = 404
+        result = Error(message="Unable to get shops")
     return result
