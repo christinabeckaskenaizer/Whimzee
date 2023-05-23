ebd8fde50e09eaa6840f63ef636c6c9532b758dd
Sina Klughardt <sina.klughardt@gmail.com>
2023-05-23 15:28:06 -0700

    Made nav bar prettier and started landing page

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,43 +1,41 @@
 import { useEffect, useState } from "react";
-import Construct from "./Construct.js";
+import Landing from "./LandingPage/Landing";
 import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./NavBar.js";
+import NavBar from "./NavBar/NavBar.js";
 
 function App() {
   const [launchInfo, setLaunchInfo] = useState([]);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
+  //     console.log("fastapi url: ", url);
+  //     let response = await fetch(url);
+  //     console.log("------- hello? -------");
+  //     let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
+  //     if (response.ok) {
+  //       console.log("got launch data!");
+  //       setLaunchInfo(data.launch_details);
+  //     } else {
+  //       console.log("drat! something happened");
+  //       setError(data.message);
+  //     }
+  //   }
+  //   getData();
+  // }, []);
 
   return (
     <BrowserRouter>
     <NavBar />
-      <div>
-        <ErrorNotification error={error} />
-        <Construct info={launchInfo} />
-      </div>
+      <div className="container">
       <Routes>
-        <Route path="/home">
+        <Route path="/home" element={<Landing />}>
+
           <Route path="home/listing"></Route>
         </Route>
 
@@ -52,6 +50,8 @@ function App() {
 
         <Route path="/checkout"></Route>
       </Routes>
+      </div>
+
     </BrowserRouter>
   );
 }
