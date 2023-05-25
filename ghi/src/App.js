1447f369e3b70816afa6fcae01340bc33aff8278
Christina Beck-Askenaizer <c.beckask@gmail.com>
2023-05-25 22:59:10 +0000

    Listing description page

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -7,15 +7,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar/NavBar.js";
 import UserAccount from "./account-components/UserAccount.js";
 import CreateShopForm from "./account-components/CreateShopForm.js";
-import ListingCard from "./ListingCard.js";
 import AllListings from "./AllListings.js";
 import Shop from "./shop-components/Shop";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUp";
 import useToken from "@galvanize-inc/jwtdown-for-react";
+import ListingDetail from "./ListingDetail";
+
 
 function App() {
   const { token } = useToken();
+  const [shopToVisit, setShopToVisit] = useState(null);
   useEffect(() => {
     console.log(token);
   }, [token]);
@@ -35,9 +37,10 @@ function App() {
             <Route path="listing" />
           </Route>
 
-          <Route path="/shops" element={<Shop />}></Route>
+          <Route path="/shops/" element={<Shop shop_id={shopToVisit} />}></Route>
 
           <Route path="/listings" element={<AllListings />} />
+          <Route path="/listings/:id" element={<ListingDetail setShopToVisit={setShopToVisit} />} />
 
           <Route path="/liked"></Route>
 
