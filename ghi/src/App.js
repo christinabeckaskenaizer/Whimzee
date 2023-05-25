09b87424c29bb996523ce9e08d9c361547834ed8
Santiago Bothe <santiagobothe@outlook.com>
2023-05-25 23:16:56 +0000

    Clean up

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,17 +6,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import NavBar from "./NavBar/NavBar.js";
 import UserAccount from "./account-components/UserAccount.js";
-import CreateShopForm from "./account-components/CreateShopForm.js";
+
+import ListingCard from "./ListingCard.js";
 import AllListings from "./AllListings.js";
-import Shop from "./shop-components/Shop";
-import LoginForm from "./LoginForm";
-import SignUpForm from "./SignUp";
-import useToken from "@galvanize-inc/jwtdown-for-react";
 import ListingDetail from "./ListingDetail";
 
+import Shop from "./shop-components/Shop";
+
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUp";
+
+import useToken from "@galvanize-inc/jwtdown-for-react";
+import useUser from "./custom-hooks/useUser";
+import useShop from "./custom-hooks/useShop";
+import useCart from "./custom-hooks/useCart";
 
 function App() {
   const { token } = useToken();
+  const { user, ids } = useUser(token);
+  const { shop } = useShop(ids);
+  const { cart } = useCart(ids);
   const [shopToVisit, setShopToVisit] = useState(null);
   useEffect(() => {
     console.log(token);
@@ -32,15 +41,25 @@ function App() {
           <Route path="/signup" element={<SignUpForm />} />
 
           <Route path="/account">
-            <Route path="" element={<UserAccount />} />
-            <Route path="shop" element={<CreateShopForm />} />
+            <Route
+              path=""
+              element={
+                <UserAccount user={user} ids={ids} shop={shop} cart={cart} />
+              }
+            />
             <Route path="listing" />
           </Route>
 
-          <Route path="/shops/" element={<Shop shop_id={shopToVisit} />}></Route>
+          <Route
+            path="/shops/"
+            element={<Shop shop_id={shopToVisit} />}
+          ></Route>
 
           <Route path="/listings" element={<AllListings />} />
-          <Route path="/listings/:id" element={<ListingDetail setShopToVisit={setShopToVisit} />} />
+          <Route
+            path="/listings/:id"
+            element={<ListingDetail setShopToVisit={setShopToVisit} />}
+          />
 
           <Route path="/liked"></Route>
 
--- ghi/src/account-components/ShopSalesList.js
+++ ghi/src/account-components/ShopSalesList.js
@@ -1,4 +1,8 @@
-export default function ShopSalesList() {
+export default function ShopSalesList({ ids }) {
+  if (!ids?.shop_id) {
+    return;
+  }
+
   return (
     <>
       <h1>Here is a list of the shop's sales.</h1>
