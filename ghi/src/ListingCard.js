f9b84c2cf293cc38562b92b4d03d07212c99fc0c
Christina <c.beckask@gmail.com>
2023-05-29 17:56:43 -0700

    fixed the a tag error that was in console for ListingCard component

--- ghi/src/ListingCard.js
+++ ghi/src/ListingCard.js
@@ -31,9 +31,9 @@ export default function ListingCard({ picture, name, isNew, price, id }) {
                         <span className="text-xl font-semibold text-gray-900 dark:text-gray-950">
                             {formatter.format(price)}
                         </span>
-                        <a
+                        <span
                             href="#"
-                            className="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                            className="text-white bg-green-800 hover:bg-green-900 focus:ring-4 font-medium rounded-lg text-sm px-2 py-2 text-center"
                         >
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ export default function ListingCard({ picture, name, isNew, price, id }) {
                                 viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
-                                className="w-6 h-6"
+                                className="w-6 h-6 "
                             >
                                 <path
                                     strokeLinecap="round"
@@ -49,7 +49,7 @@ export default function ListingCard({ picture, name, isNew, price, id }) {
                                     d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                                 />
                             </svg>
-                        </a>
+                        </span>
                     </div>
                 </Link>
             </div >
