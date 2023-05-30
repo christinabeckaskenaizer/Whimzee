cdae624b68250320a99b462fa26171c444ee9b61
Sina Klughardt <sina.klughardt@gmail.com>
2023-05-30 11:53:13 -0700

    Search Items by name is working properly

--- ghi/src/NavBar/SearchBar.js
+++ ghi/src/NavBar/SearchBar.js
@@ -27,7 +27,12 @@ function SearchBar() {
         const category = e.target.value;
         setSearchedCategory(category)
     };
-    console.log(searchedCategory)
+
+    const handleSearchedItemName = async (e) => {
+        const name = e.target.value.toLowerCase();
+        setSearchedItemName(name)
+    }
+    console.log(searchedItemName)
 
     return (
       <form>
@@ -43,7 +48,7 @@ function SearchBar() {
         })}
         </select>
           <div className="relative w-full">
-            <input type="search" id="search-dropdown" className="block p-2.5 w-96 z-30 text-sm text-black bg-white rounded-r-lg border-l-white border-l-2 border border-white dark:placeholder-gray-500" placeholder="Search tshirts, paintings, cups,..." required/>
+            <input onChange={handleSearchedItemName} type="search" id="search-dropdown" className="block p-2.5 w-96 z-30 text-sm text-black bg-white rounded-r-lg border-l-white border-l-2 border border-white dark:placeholder-gray-500" placeholder="Search tshirts, paintings, cups,..." value={searchedItemName} required/>
             <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-black dark:bg-black dark:hover:bg-grey-400 dark:focus:ring-black">
                 <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 <span className="sr-only">Search</span>
