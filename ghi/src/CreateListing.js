03ddf181c08cf6d535a88bc9200f35e39190450a
Christina <c.beckask@gmail.com>
2023-05-29 17:49:00 -0700

    set USED? toggle to be correct upon creation of listing

--- ghi/src/CreateListing.js
+++ ghi/src/CreateListing.js
@@ -10,7 +10,7 @@ export default function MyModal() {
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState('');
 
-    const [toggled, setToggled] = useState(false);
+    const [toggled, setToggled] = useState(true);
     const [categories, setCategories] = useState([]);
     const [category, setCategory] = useState();
 
@@ -150,7 +150,7 @@ export default function MyModal() {
                                             <label className="relative inline-flex items-center mr-5 cursor-pointer">
                                                 <input onChange={toggleUsed} type="checkbox" value="" className="sr-only peer" />
                                                 <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-                                                <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Used?</span>
+                                                <span className="ml-3 text-sm font-medium text-gray-900">Used?</span>
                                             </label>
                                         </div>
                                     </form>
