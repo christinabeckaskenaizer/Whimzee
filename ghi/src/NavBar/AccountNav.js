7d68ceba0bbad3af124ccce1642499e8d528de26
Sina Klughardt <sina.klughardt@gmail.com>
2023-06-07 09:40:34 -0700

    Deleted active on Menuitem because we didn't use it

--- ghi/src/NavBar/AccountNav.js
+++ ghi/src/NavBar/AccountNav.js
@@ -67,7 +67,7 @@ export default function AccountNav({ token }) {
                   <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
                       <Menu.Item>
-                        {({ active }) => (
+                        {() => (
                           <div className="py-1" role="none">
                             <div
                               className="text-gray-700 block px-4 py-1 text-sm"
@@ -87,7 +87,7 @@ export default function AccountNav({ token }) {
                         )}
                       </Menu.Item>
                       <Menu.Item>
-                        {({ active }) => (
+                        {() => (
                           <NavLink
                             to="/account"
                             className="text-gray-700 block px-4 py-2 text-sm md:dark:text-black md:hover:text-green-800"
