7a461f1b60b1d715bc9b75e1c0fcff6b24ac0c52
Christina <c.beckask@gmail.com>
2023-06-08 14:34:49 -0700

    linting issues

--- ghi/src/listing-components/ListingCard.js
+++ ghi/src/listing-components/ListingCard.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ListingCard({ picture, name, isNew, price, id, wishlist, fetchWL, token, ids, changeWishlist }) {
