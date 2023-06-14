30dfde0829b40112816e1c3670aec01d7949aa54
Santiago Bothe <santiagobothe@outlook.com>
2023-06-14 12:40:12 -0600

    linting fixes

--- ghi/src/account-components/AccountOrderHistory.js
+++ ghi/src/account-components/AccountOrderHistory.js
@@ -49,6 +49,7 @@ export default function AccountOrderHistory({ history }) {
                   </div>
                 );
               }
+              return <></>;
             })}
           </div>
         ) : (
